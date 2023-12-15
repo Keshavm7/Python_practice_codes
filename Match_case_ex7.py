40c6e33c697f6384738887af1a6d8e700f140ce1
Keshavm7 <keshavmirjapurkar7@gmail.com>
2023-12-15 16:36:58 +0530

    code updated

--- Match_case_ex7.py
+++ Match_case_ex7.py
@@ -1,1 +1,13 @@
 # Program for identifying entered day is Weekday/Working Day or Weekend or Holiday with Match Case Statment
+wkd=input("Enter Weekday Name: ")
+print("*"*50)
+match(wkd.upper()[0:3]):
+      case "MON"|"TUE"|"WED"|"THU"|"FRI":
+         print("{} is Working day".format(wkd.capitalize()))
+      case "SAT":
+         print("{} is Weekend".format(wkd.capitalize()))
+      case "SUN":
+         print("{} is HOLIDAY".format(wkd.capitalize()))
+      case _:
+         print("The enterd day name is incorrect!!!! Try again")
+print("*"*50)
